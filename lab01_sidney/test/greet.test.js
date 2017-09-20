86ca5a271b7fb1951c11434320db6db0ba4abe3e
sgordon834 <sgordon834@gmail.com>
2017-09-19 23:02:34 -0700

    added greet test

---
+++ lab01_sidney/test/greet.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const expect = require('expect');
+
+const greet = require('../lib/greet.js');
+
+
+describe('greet', function () {
+  it('should save without error', function () {
+  });
+});
